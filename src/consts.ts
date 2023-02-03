d7167a7eb741b832bc709d4f28a5f98fd194ce57
jamilsinno <jamil.sinno@gmail.com>
2023-02-02 21:01:25 -0400

    initialization of website and changed website title and description

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'SINNO TECHNOLOGIES';
+export const SITE_DESCRIPTION = `Unlock Your Business Potential with Innovative, Accessible Technology Solutions`;
